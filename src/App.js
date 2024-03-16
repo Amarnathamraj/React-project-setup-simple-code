c9e125b3cf9552c46421c463e7d5bc03ec2d5e01
Amarnathamraj <143314741+Amarnathamraj@users.noreply.github.com>
2024-03-16 20:09:44 +0530

    Update App.js
    wrote custom html tag to use JSX file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-
+import ExpenseItem from "./components/ExpenseItem";
 function App() {
   return (
     <div>
       <h1>Let's get Started</h1>
-     <p>I am learning React</p>
+     <ExpenseItem></ExpenseItem>
     </div>
   );
 }
